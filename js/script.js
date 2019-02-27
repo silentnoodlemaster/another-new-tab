747531ed713e41c29b18ce4c1d2cc5a4c23ba6c5
ben <lonnqvistben@gmail.com>
2019-02-27 14:39:07 +0200

    style changes

--- js/script.js
+++ js/script.js
@@ -17,15 +17,13 @@ function time() {
   var today = new Date();
   var h = today.getHours();
   var m = today.getMinutes();
-  var s = today.getSeconds();
   h = checkTime(h);
   m = checkTime(m);
-  s = checkTime(s);
   document.getElementById('time').innerHTML =
-  h + ':' + m + ':' + s;
+  h + ':' + m ;
   var t = setTimeout(time, 500);
 }
 function checkTime(i) {
-  if (i < 10) {i = '0' + i};
+  if (i < 10) {i = '0' + i;}
   return i;
 }
